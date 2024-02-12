7b5188fb04718cda491b09ef26ed9f95adb3926c
oclk <oclk@outlook.com.tr>
2024-02-12 23:23:02 +0300

    feat: Created TcpServer.cs

---
+++ Source/SocketR/TcpServer.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+
+namespace SocketR;
+
+public class TcpServer : ITcpServer
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ITcpServer OnClientConnected(Action<TcpClient> clientConnectedAction)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ITcpServer OnDataReceived(Func<ArraySegment<byte>, Task> dataReceivedFunc)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ITcpServer OnError(Action<Exception> errorAction)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StartAsync(string ipAddress, int port, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
