ca76590649ad3644a6fb28920616acd8f5b92c05
oclk <oclk@outlook.com.tr>
2024-02-12 23:36:06 +0300

    feat: Added ReceiveDataAsync and AcceptClientsAsync methods to the TcpServer.cs

--- Source/SocketR/TcpServer.cs
+++ Source/SocketR/TcpServer.cs
@@ -4,9 +4,22 @@ namespace SocketR;
 
 public class TcpServer : ITcpServer
 {
+    private TcpListener? _listener;
+    private CancellationTokenSource _cancellationTokenSource;
+    private Action<TcpClient> _clientConnectedAction;
+    private Func<ArraySegment<byte>, Task> _dataReceivedFunc;
+    private Action<Exception> _errorAction;
+
+    public TcpServer() => _cancellationTokenSource = new CancellationTokenSource();
+
     public void Dispose()
     {
-        throw new NotImplementedException();
+        _cancellationTokenSource.Dispose();
+        if (_listener != null)
+        {
+            _listener.Stop();
+            _listener = null;
+        }
     }
 
     public ITcpServer OnClientConnected(Action<TcpClient> clientConnectedAction)
@@ -32,5 +45,45 @@ public class TcpServer : ITcpServer
     public Task StopAsync()
     {
         throw new NotImplementedException();
+    }
+
+    private async Task AcceptClientsAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var client = await _listener.AcceptTcpClientAsync(cancellationToken);
+            _clientConnectedAction?.Invoke(client);
+
+            _ = Task.Run(async () => await ReceiveDataAsync(client, cancellationToken), cancellationToken);
+        }
+    }
+
+    private async Task ReceiveDataAsync(TcpClient client, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var stream = client.GetStream();
+            var buffer = new byte[1024];
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                var data = new ArraySegment<byte>(buffer, 0, bytesRead);
+                await _dataReceivedFunc.Invoke(data);
+            }
+        }
+        catch (Exception ex)
+        {
+            _errorAction?.Invoke(ex);
+        }
+        finally
+        {
+            client.Close();
+        }
     }
 }
