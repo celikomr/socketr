e2b5afa4c8257217a59fb817ba2664c67aa5d886
oclk <oclk@outlook.com.tr>
2024-02-12 23:20:08 +0300

    feat: Added OnClientConnected, OnDataReceived and OnError to the ITcpServer.cs

--- Source/SocketR/ITcpServer.cs
+++ Source/SocketR/ITcpServer.cs
@@ -1,7 +1,12 @@
-﻿namespace SocketR;
+﻿using System.Net.Sockets;
+
+namespace SocketR;
 
 public interface ITcpServer : IDisposable
 {
     Task StartAsync(string ipAddress, int port, CancellationToken cancellationToken = default);
     Task StopAsync();
+    ITcpServer OnClientConnected(Action<TcpClient> clientConnectedAction);
+    ITcpServer OnDataReceived(Func<ArraySegment<byte>, Task> dataReceivedFunc);
+    ITcpServer OnError(Action<Exception> errorAction);
 }
